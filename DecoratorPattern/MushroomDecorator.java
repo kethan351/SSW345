09db5b49c232a3ccdb982869d8f3ffabe190168b
kethan351 <101610058+kethan351@users.noreply.github.com>
2023-04-06 16:48:33 -0400

    Add files via upload

---
+++ DecoratorPattern/MushroomDecorator.java
@@ -0,0 +1,14 @@
+
+public class MushroomDecorator extends ToppingDecorator{
+	public MushroomDecorator(Pizza pizza) {
+		super(pizza);
+	}
+	@Override
+	public String getDescription() {
+		return pizza.getDescription() + " mushrooms";
+	}
+	@Override
+	public double getCost() {
+		return pizza.getCost() + 1.25;
+	}
+}
